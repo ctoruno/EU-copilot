e1b4d8662f8dac8a5e4bd3289dba86444a25a824
Carlos A. Toruño P. <carlos.toruno@gmail.com>
2024-05-20 16:04:12 -0400

    update - pass check

--- pages/5_Dashboard.py
+++ pages/5_Dashboard.py
@@ -13,36 +13,7 @@ import streamlit as st
 import plotly.express as px
 import tools.viz_tools as viz
 
-# Defining a function to check for password
-def check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
-
-if check_password():
+if viz.check_password():
 
     # Page config
     st.set_page_config(
@@ -351,7 +322,9 @@ if check_password():
                 provides an equal and fair treatment.
             </p>
             <p class='jtext'>
-                Feel free to play with the data below:
+                Feel free to play with the data below. Don't forget that individual indicators have been re-aligned to higher
+                values are positive outcomes for the Rule of Law in a country, while lower values are associated to negative
+                perceptions of the Rule of Law.
             </p>
             """, 
             unsafe_allow_html=True
--- tools/viz_tools.py
+++ tools/viz_tools.py
@@ -9,6 +9,35 @@ This version:   May 20th, 2024
 import plotly.express as px
 import streamlit as st
 
+# Defining a function to check for password
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
 def genBars(data, cpal):
     fig = px.bar(
         data, 
